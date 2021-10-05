94842382863969eb1c06f949645ad526e844cad6
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-10-05 09:38:03 -0600

    tipo de datos

---
+++ Sintaxis basica/tipoDatos.py
@@ -0,0 +1,9 @@
+n = 1
+bool = False
+hola = "Soy Joan"
+n2 = 1.3
+
+print("Este es un entero",n)
+print("este es un tipo booleano",bool)
+print("Esta es una cadena de caracteres",hola)
+print("este es un valor flotante",n2)
