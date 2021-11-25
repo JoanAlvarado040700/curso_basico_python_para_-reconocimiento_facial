2652de6100118f8daa9a7987d0ace251819e6509
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-11-25 02:37:07 -0600

    Bucles

--- Ejercicio_monedas/monedascontorno/Contadormonedas.py
+++ Ejercicio_monedas/monedascontorno/Contadormonedas.py
@@ -5,9 +5,9 @@ import numpy as np
 #------------------------------------------------#
 
 #Funciones <------------
-vargaus = 3
-varkernle = 3
-original = cv2.imread("D:/Proyectos python/ejercicio_monedas/monedascontorno/monedas.jpg")
+vargaus = 7
+varkernle = 5
+original = cv2.imread("D:/Proyectos python/ejercicio_monedas/monedascontorno/cordobas.jpg")
 gris = cv2.cvtColor(original,COLOR_BGR2GRAY)
 gaus = GaussianBlur(gris,(vargaus,varkernle),0)
 cany = cv2.Canny(gaus,60,100)
@@ -17,7 +17,7 @@ kernel = np.ones((varkernle,varkernle),np.uint8)
 cierre = cv2.morphologyEx(cany,cv2.MORPH_CLOSE,kernel)
 
 contornos,jerarquias = cv2.findContours(cierre,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE)
-#Imprimir resultados
+#Imprime la cantidad de contornos encontrados 
 print("Monedas encontradas: {} ".format(len(contornos)))
 #Dibujar contornos 
 cv2.drawContours(original,contornos,-1,(181, 56, 34),2)
@@ -27,6 +27,7 @@ cv2.drawContours(original,contornos,-1,(181, 56, 34),2)
 cv2.imshow("grises",gris) 
 cv2.imshow("Modelado Gaus", gaus)
 cv2.imshow("Modelado cany", cany)
+cv2.imshow("cierre",cierre)
 cv2.imshow("Resultados",original)
 
 
