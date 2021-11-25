a407c09827c2c6879d51446957c9682d9eb544af
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-11-25 01:45:28 -0600

    Bucles

--- Ejercicio_monedas/monedascontorno/Contadormonedas.py
+++ Ejercicio_monedas/monedascontorno/Contadormonedas.py
@@ -1,4 +1,23 @@
-from cv2 import cv2, imread
-import numpy as np
+from cv2 import COLOR_BGR2GRAY, Canny, GaussianBlur, cv2, imshow
 
-original = imread("D:/Proyectos python/ejercicio_monedas/monedascontorno/monedas.jpg")
+#Funciones
+
+vargaus = 3
+varkernle = 3
+original = cv2.imread("D:/Proyectos python/ejercicio_monedas/monedascontorno/monedas.jpg")
+gris = cv2.cvtColor(original,COLOR_BGR2GRAY)
+gaus = GaussianBlur(gris,(vargaus,varkernle),10)
+cany = cv2.Canny(gaus,60,100)
+
+
+
+#Resultados <----------
+
+#cv2.imshow("grises",gris) | 
+cv2.imshow("Modelado Gaus", gaus)
+cv2.imshow("Modelado cany", cany)
+
+cv2.imshow("Original",original)
+
+
+cv2.waitKey(0)
