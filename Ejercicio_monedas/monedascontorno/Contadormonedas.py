043cf91d3fe48c82f552c0c555c0a160d1035f4e
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-11-25 02:23:55 -0600

    Bucles

--- Ejercicio_monedas/monedascontorno/Contadormonedas.py
+++ Ejercicio_monedas/monedascontorno/Contadormonedas.py
@@ -1,23 +1,33 @@
-from cv2 import COLOR_BGR2GRAY, Canny, GaussianBlur, cv2, imshow
+#Librerias usadas <---------
+from cv2 import CHAIN_APPROX_SIMPLE, COLOR_BGR2GRAY, RETR_EXTERNAL, GaussianBlur, cv2
+import numpy as np
 
-#Funciones
+#------------------------------------------------#
 
+#Funciones <------------
 vargaus = 3
 varkernle = 3
 original = cv2.imread("D:/Proyectos python/ejercicio_monedas/monedascontorno/monedas.jpg")
 gris = cv2.cvtColor(original,COLOR_BGR2GRAY)
-gaus = GaussianBlur(gris,(vargaus,varkernle),10)
+gaus = GaussianBlur(gris,(vargaus,varkernle),0)
 cany = cv2.Canny(gaus,60,100)
 
+kernel = np.ones((varkernle,varkernle),np.uint8)
+
+cierre = cv2.morphologyEx(cany,cv2.MORPH_CLOSE,kernel)
+
+contornos,jerarquias = cv2.findContours(cierre,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE)
+#Imprimir resultados
+print("Monedas encontradas: {} ".format(len(contornos)))
+#Dibujar contornos 
+cv2.drawContours(original,contornos,-1,(181, 56, 34),2)
 
 
 #Resultados <----------
-
-#cv2.imshow("grises",gris) | 
+cv2.imshow("grises",gris) 
 cv2.imshow("Modelado Gaus", gaus)
 cv2.imshow("Modelado cany", cany)
-
-cv2.imshow("Original",original)
+cv2.imshow("Resultados",original)
 
 
 cv2.waitKey(0)
