266055a54d20eb5a445a592091228db5023cd2c9
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-11-27 00:51:48 -0600

    Bucles

---
+++ Contador_con_camara/contador_con_camara.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+
+captura = cv.VideoCapture(0)
+if not captura.isOpened():
+    print("No se encontro una camara")
+    exit()
+while True:
+    _,Camara = captura.read()
+  #  grises = cv.cvtColor(Camara,cv.COLOR_BGR2GRAY)
+   # _,umbral = cv.threshold(grises,100,255,cv.THRESH_BINARY)
+   # contorno, jerarquia = cv.findContours(umbral,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+   # cv.drawContours(Camara,contorno,-1,(255, 0, 0),1)
+    cv.imshow("Camara en vivo",Camara)
+    
+    if cv.waitKey(1) == ord("q"):
+        break
+captura.release()
+cv.destroyAllWindows()
+
+
